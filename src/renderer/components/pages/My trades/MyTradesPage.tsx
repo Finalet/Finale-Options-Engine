2671a7ea7a8a78b84ff0c66bd580740e90df8c90
Grant Oganyan <grant1599@mail.ru>
2024-04-20 00:23:52 -0400

    Update MyTradesPage.tsx

--- src/renderer/components/pages/My trades/MyTradesPage.tsx
+++ src/renderer/components/pages/My trades/MyTradesPage.tsx
@@ -125,16 +125,32 @@ const columns: any = [
     },
   }),
   columnHelper.accessor((row) => row.spreadLive?.returnAtExpiration, {
-    id: 'return',
-    header: ({ column }) => <DataTableColumnHeader column={column} title="Return" />,
+    id: 'change',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Change" />,
     cell: ({ row }) => {
-      const currentReturn = getCurrentReturn(row.original);
+      const currentReturn = getCurrentChange(row.original);
       if (currentReturn === undefined) return <Placeholder />;
       return <span className={`${currentReturn > 0 ? 'text-primary' : currentReturn === 0 ? 'text-foreground' : 'text-red-600'} font-semibold`}>{currentReturn}%</span>;
     },
     sortingFn: (a, b) => {
-      const aReturn = getCurrentReturn(a.original);
-      const bReturn = getCurrentReturn(b.original);
+      const aReturn = getCurrentChange(a.original);
+      const bReturn = getCurrentChange(b.original);
+      if (aReturn === undefined || bReturn === undefined) return 0;
+      return aReturn - bReturn;
+    },
+  }),
+  columnHelper.accessor((row) => row.spreadLive?.returnAtExpiration, {
+    id: 'return',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Return" />,
+    cell: ({ row }) => {
+      if (row.original.spreadLive === undefined) return <Placeholder />;
+      const earned = (100 * row.original.quantity * (row.original.spreadAtOpen.price - row.original.spreadLive.price)) / row.original.spreadAtOpen.stats.collateral;
+      const returnPercent = roundTo(100 * earned);
+      return <span className={`${returnPercent > 0 ? 'text-primary' : returnPercent === 0 ? 'text-foreground' : 'text-red-600'} font-semibold`}>{returnPercent}%</span>;
+    },
+    sortingFn: (a, b) => {
+      const aReturn = getCurrentChange(a.original);
+      const bReturn = getCurrentChange(b.original);
       if (aReturn === undefined || bReturn === undefined) return 0;
       return aReturn - bReturn;
     },
@@ -145,7 +161,7 @@ const Placeholder = () => {
   return <Skeleton className="w-10 h-5" />;
 };
 
-const getCurrentReturn = (trade: CallCreditSpreadTrade): number | undefined => {
+const getCurrentChange = (trade: CallCreditSpreadTrade): number | undefined => {
   const openPrice = trade.spreadAtOpen.price;
   const currentPrice = trade.spreadLive?.price;
   if (currentPrice === undefined) return undefined;
